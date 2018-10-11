9bba7807ff603e1f9e917d9fcf22be4daebfbfe3
SebastianBrajlich <sebastian.brajlich@gmail.com>
2018-10-11 11:13:11 +0200

    Modify LogoutServiceTest

--- src/test/java/com/codecool/queststore/controller/server/LogoutServiceTest.java
+++ src/test/java/com/codecool/queststore/controller/server/LogoutServiceTest.java
@@ -13,12 +13,12 @@ public class LogoutServiceTest {
 
     @Test
     void checksIfsessionExistswhenLogout() {
-        SessionPool sessionPool = new SessionPool();
-        Session session = sessionPool.getNewSession(12);
+        //SessionPool sessionPool = new SessionPool();
+        Session session = SessionPool.getNewSession(12);
         HttpCookie cookie = new HttpCookie("ID-Session", session.getUuid().toString());
 
         LogoutService logoutService = new LogoutService(cookie);
 
-        Assertions.assertFalse(sessionPool.sessions.contains(session));
+        Assertions.assertFalse(SessionPool.sessions.contains(session));
     }
 }
