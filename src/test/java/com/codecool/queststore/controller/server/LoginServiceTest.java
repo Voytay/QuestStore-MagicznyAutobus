b1208fab154fd7d544e7b24c4be1b31540c7c03b
SebastianBrajlich <sebastian.brajlich@gmail.com>
2018-10-11 10:10:09 +0200

    Create LoginServiceTest and LoginHandlerTest classes that tests funcionality in LoginService and LoginHandler classes.

--- src/test/java/com/codecool/queststore/controller/server/LoginServiceTest.java
+++ src/test/java/com/codecool/queststore/controller/server/LoginServiceTest.java
@@ -2,6 +2,8 @@ package com.codecool.queststore.controller.server;
 
 import com.codecool.queststore.controller.server.service.LoginService;
 import com.codecool.queststore.model.Login;
+import com.codecool.queststore.model.server.session.Session;
+import com.codecool.queststore.model.server.session.SessionPool;
 import com.codecool.queststore.model.user.Role;
 import com.codecool.queststore.model.user.User;
 import org.junit.jupiter.api.Test;
@@ -12,14 +14,30 @@ import static org.mockito.Mockito.*;
 
 class LoginServiceTest {
     String name = "Seba";
-    String password = "dupa";
+    String password = "Seba";
+    String surname = "Brajlich";
+    String email = "sb@gmail.com";
+    String address = "NeverLand";
+    int id = 12;
+    Role role = Role.ADMIN;
 
     @Test
     void testLoginServiceConstructor(){
-        Login login = mock(Login.class);
-        when(login.SignIn()).thenReturn(null);
-        LoginService loginService = new LoginService(name, password);
+        LoginService loginService = spy(new LoginService(name, password));
+        doReturn(null).when(loginService).SignIn();
+        User user = loginService.SignIn();
+        loginService.sessionCreationIfUserValid(user);
         assertEquals(null, loginService.getSession());
+    }
+
+    @Test
+    void ifUserHasSession(){
+        User user = new User(name, surname, email, address, id, role);
+        LoginService loginService = spy(new LoginService(name, password));
+        doReturn(user).when(loginService).SignIn();
+        User newUser = loginService.SignIn();
+        loginService.sessionCreationIfUserValid(newUser);
+        assertTrue(SessionPool.sessions.contains(loginService.getSession()));
     }
 
 
--- src/test/java/com/codecool/queststore/controller/server/httphandler/LoginHandlerTest.java
+++ src/test/java/com/codecool/queststore/controller/server/httphandler/LoginHandlerTest.java
@@ -1,11 +1,17 @@
 package com.codecool.queststore.controller.server.httphandler;
 
 import com.codecool.queststore.controller.server.httphandler.LoginHandler;
+import com.codecool.queststore.model.server.session.Session;
+import com.codecool.queststore.model.server.session.SessionPool;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Spy;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,10 +30,24 @@ public class LoginHandlerTest {
     @Test
     void testIfHandlePostMethodGetsCalled() throws Exception {
         HttpExchange mockHttpExchange = mock(HttpExchange.class);
-        when(mockHttpExchange.getRequestMethod()).thenReturn("POST");
-
+        doReturn("POST").when(mockHttpExchange).getRequestMethod();
         doNothing().when(loginHandler).handlePost(mockHttpExchange);
         loginHandler.handle(mockHttpExchange);
         verify(loginHandler).handlePost(mockHttpExchange);
     }
+
+    @Test
+    void testIfRedirectMethodGetsBalld() throws Exception {
+        Headers headers = new Headers();
+        Session session = SessionPool.getNewSession(12);
+        headers.add("Cookie", session.getUuid().toString());
+        HttpExchange mockHttpExchange = mock(HttpExchange.class);
+        doReturn(headers).when(mockHttpExchange).getRequestHeaders();
+        doNothing().when(loginHandler).redirect(mockHttpExchange, "/profile/");
+        loginHandler.handleSession(mockHttpExchange);
+        verify(loginHandler).redirect(mockHttpExchange,"/profile/");
+
+    }
+
+
 }
